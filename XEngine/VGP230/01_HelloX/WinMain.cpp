df54af2ae53a9c64cd4d3d3dbcdb17556f18376d
varuka999 <vjkpokemon@yahoo.com>
2025-10-07 21:45:23 -0700

    HelloX In-Class Progress

---
+++ XEngine/VGP230/01_HelloX/WinMain.cpp
@@ -0,0 +1,72 @@
+#include "XEngine.h"
+
+X::TextureId imageID = 0;
+X::Math::Vector2 imagePos = { 0.0f, 0.0f };
+float imageRot = 0.0f;
+
+void GameInit()
+{
+	XLOG("Game Init");
+	imageID = X::LoadTexture("mario.png");
+	imagePos.x = X::GetScreenWidth() * 0.5f;
+	imagePos.y = X::GetScreenHeight() * 0.5f;
+}
+
+void GameRender()
+{
+	X::DrawSprite(imageID, imagePos, imageRot);
+}
+
+bool GameLoop(float deltaTime)
+{
+	//XLOG("Game Loop");
+	//std::cout << "Game Loop";
+	const float moveSpeed = 500.0f;
+	const float rotSpeed = 20.0f;
+	if (X::IsKeyDown(X::Keys::UP))
+	{
+		imagePos.y -= moveSpeed * deltaTime;
+	}
+	if (X::IsKeyDown(X::Keys::RIGHT))
+	{
+		imagePos.x += moveSpeed * deltaTime;
+	}
+	if (X::IsKeyDown(X::Keys::LEFT))
+	{
+		imagePos.x -= moveSpeed * deltaTime;
+	}
+	if (X::IsKeyDown(X::Keys::DOWN))
+	{
+		imagePos.y += moveSpeed * deltaTime;
+	}
+	if (X::IsKeyDown(X::Keys::E))
+	{
+		imageRot += rotSpeed * deltaTime;
+	}
+	if (X::IsKeyDown(X::Keys::Q))
+	{
+		imageRot -= rotSpeed * deltaTime;
+	}
+
+	GameRender();
+	return X::IsKeyPressed(X::Keys::ESCAPE);
+}
+
+void GameCleanup()
+{
+	XLOG("Game Cleanup");
+}
+
+
+int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	X::Start("xconfig.json");
+	GameInit();
+
+	X::Run(GameLoop);
+
+	GameCleanup();
+	X::Stop();
+
+	return 0;
+}
